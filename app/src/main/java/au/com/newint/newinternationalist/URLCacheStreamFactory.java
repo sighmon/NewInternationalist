282e5ef330fa035dabdf59f1e327ff6f3533f3ce
pix <thatpixguy@gmail.com>
2015-04-30 18:09:16 +0930

    enable URLCacheStreamFactory to accept HttpUriRequests for more complicated post requests

--- app/src/main/java/au/com/newint/newinternationalist/URLCacheStreamFactory.java
+++ app/src/main/java/au/com/newint/newinternationalist/URLCacheStreamFactory.java
@@ -2,32 +2,59 @@ package au.com.newint.newinternationalist;
 
 import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
  * Created by pix on 27/02/15.
  */
 public class URLCacheStreamFactory extends CacheStreamFactory {
-    private final URL sourceURL;
+    //private final URL sourceURL;
+    private final HttpUriRequest sourceURIRequest;
 
     URLCacheStreamFactory(URL sourceURL) {
         this(sourceURL,null,null);
     }
 
+    //TODO: can this be DRYer without angering the compiler?
+
     // fallback will normally be null
     URLCacheStreamFactory(URL sourceURL, CacheStreamFactory fallback, String name) {
+        super(fallback, name == null ? "net" : name);
+
+        HttpUriRequest httpUriRequest;
+
+        try {
+            httpUriRequest = new HttpGet(sourceURL.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            httpUriRequest = null;
+        }
+
+        this.sourceURIRequest = httpUriRequest;
+    }
+
+    URLCacheStreamFactory(HttpUriRequest sourceURIRequest, CacheStreamFactory fallback, String name) {
         super(fallback, name==null?"net":name);
 
-        this.sourceURL = sourceURL;
+        this.sourceURIRequest = sourceURIRequest;
     }
 
     @Override
     public String toString() {
-        return "URLCacheStreamFactory["+sourceURL.getPath()+"]";
+        return "URLCacheStreamFactory["+sourceURIRequest+"]";
     }
 
     // Q: should we automatically fail if on the UI thread? is returning null enough?
@@ -35,15 +62,19 @@ public class URLCacheStreamFactory extends CacheStreamFactory {
     // A: bad idea unless we need it, as it might hide design flaws
     @Override
     protected InputStream createCacheInputStream() {
-        Log.i("URLCacheStreamFactory", "createCacheInputStream() ["+sourceURL+"]");
+        Log.i("URLCacheStreamFactory", "createCacheInputStream() ["+sourceURIRequest+"]");
 
         HttpURLConnection urlConnection = null;
 
         try {
-            urlConnection = (HttpURLConnection) sourceURL.openConnection();
-            return urlConnection.getInputStream();
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            HttpContext ctx = new BasicHttpContext();
+            ctx.setAttribute(ClientContext.COOKIE_STORE, Publisher.INSTANCE.cookieStore);
+            HttpResponse response = httpclient.execute(sourceURIRequest,ctx);
+            return response.getEntity().getContent();
+
         } catch (IOException e) {
-            Log.e("URLCacheStreamFactory", "Error opening source URL: " + sourceURL.toString());
+            Log.e("URLCacheStreamFactory", "Error opening source URL: " + sourceURIRequest.toString());
             //e.printStackTrace();
         }
         return null;
